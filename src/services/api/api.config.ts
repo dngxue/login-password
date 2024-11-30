8c56a52acc546db7261affed6dfbee43d36eda5c
Sergew_RMZ <serge15games@gmail.com>
2024-11-30 02:59:09 -0600

    env

--- src/services/api/api.config.ts
+++ src/services/api/api.config.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const Api = axios.create({
   // Desarrollo baseURL: `http://localhost:8080/api/auth`,
-  baseURL: process.env.VUE_APP_API_URL
+  baseURL: process.env.APP_API_URL
 });
 
 export default Api;
